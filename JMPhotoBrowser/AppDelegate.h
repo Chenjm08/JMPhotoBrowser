096d57f34bb5fe0ec26deea3c57ac4c10701d0a3
Chenjm08 <459071866@qq.com>
2015-05-11 23:54:40 +0800

    Initial Commit

---
+++ JMPhotoBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JMPhotoBrowser
+//
+//  Created by chenjiemin on 15/5/11.
+//  Copyright (c) 2015年 chenjiemin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JMPhotoBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JMPhotoBrowser
+//
+//  Created by chenjiemin on 15/5/11.
+//  Copyright (c) 2015年 chenjiemin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
